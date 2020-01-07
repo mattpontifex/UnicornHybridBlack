6e0e15984a0c9fe1fa2d79cf36e535f84f9bb547
Matthew B. Pontifex <31024683+mattpontifex@users.noreply.github.com>
2020-01-07 09:58:22 -0500

    Add files via upload

--- EEGLAB Plugin/eegplugin_loadunicornhybridblack.m
+++ EEGLAB Plugin/eegplugin_loadunicornhybridblack.m
@@ -3,4 +3,4 @@ function eegplugin_loadunicornhybridblack(fig,try_strings,catch_strings)
 % create menu
 toolsmenu1 = findobj(fig, 'tag', 'import data');
 
-uimenu( toolsmenu1, 'label', 'From Unicorn Hybrid Black', 'separator','on','callback', '[EEG LASTCOM]=pop_loadunicornhybridblack;  [ALLEEG EEG CURRENTSET] = eeg_store(ALLEEG, EEG, CURRENTSET); eeglab redraw');
+uimenu( toolsmenu1, 'label', 'From Unicorn Hybrid Black', 'separator','on','callback', '[EEG LASTCOM]=pop_loadunicornhybridblack; [ALLEEG EEG CURRENTSET] = eeg_store(ALLEEG, EEG, CURRENTSET); eeglab redraw');
