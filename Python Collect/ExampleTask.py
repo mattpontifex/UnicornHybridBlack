97ad98d9fec5f944a03f60d157fad782da386672
Matthew B. Pontifex <31024683+mattpontifex@users.noreply.github.com>
2020-01-09 15:44:28 -0500

    Add files via upload

--- Python Collect/ExampleTask.py
+++ Python Collect/ExampleTask.py
@@ -1,51 +1,118 @@
+import numpy
+import random 
+import pandas
 from psychopy import visual, core, event
 
-trialsequence = [0.6, 0.6, 1.2, 0.6, 1.2, 0.6]
-stimdelayinsec = 0.150
+targetsize = 1.2
+nontargetsize = 0.6
+totaltrials = 20
+targetprobability = 0.2
+
+# create target arrays
+targettrials = [targetsize] * int(numpy.floor(totaltrials * targetprobability))
+nontargettrials = [nontargetsize] * (int(numpy.floor(totaltrials * (1-targetprobability)))-2)
+trialsequence = targettrials + nontargettrials
+random.shuffle(trialsequence)
+trialsequence = [nontargetsize, nontargetsize] + trialsequence
+
+# create event codes
+eventcodes = [5] * len(trialsequence)
+for incrX in range(len(trialsequence)):
+    if (trialsequence[incrX] == targetsize):
+        eventcodes[incrX] = 10
+
+# stimulus parameters
 stimdurationinsec = .300
-durationinsec = 1.0
-cumulativeTime = core.Clock(); cumulativeTime.reset()
-participantwin = visual.Window(size = (500,500), fullscr = False, screen = 0, allowGUI = False, allowStencil = False, monitor = 'testMonitor', color = '#000000', colorSpace = 'rgb')
+trialdurationinsec = 1.2
+refreshrate = 1 / 60.0
+participantkeys = ['1', '2', '3', '4']
+
+
+# task setup
+cumulativeTime = core.Clock()
+participantwin = visual.Window(size = (800,800), fullscr = False, screen = 0, allowGUI = False, allowStencil = False, monitor = 'testMonitor', color = '#000000', colorSpace = 'rgb')
 
 participantwin.flip()   
 
 targetstimuli = visual.Circle(participantwin, radius=[0.1, 0.1], pos = [0,0], fillColor='#FFFFFF', lineColor='#FFFFFF', autoLog=False)
+responsetracking = []
+      
+event.BuilderKeyResponse()
+event.clearEvents()                        
+                              
+elapsedTime = core.Clock(); 
+cumulativeTime.reset()
+trial = 0
+stimulusisbeingdisplayed = False
 
-for countTrials in range(len(trialsequence)):
-        
-    event.BuilderKeyResponse()
-    event.clearEvents()
-    runtimeTime = core.Clock(); runtimeTime.reset()
+continueTrial = True
+continueExperiment = True
+while continueExperiment:                              
+          
+    if (trial >= len(trialsequence)):
+        continueExperiment = False
+        break                    
+         
+    if event.getKeys(["escape", "q"]): # Check for kill keys
+        continueExperiment = False
+        break                 
+                              
+    continueTrial = True
     
-    stimradiusInitial = ((trialsequence[countTrials]/500)*100, (trialsequence[countTrials]/500)*100)
+    # set the new stimulus
+    stimradiusInitial = ((trialsequence[trial]/500)*100, (trialsequence[trial]/500)*100)
     targetstimuli.setRadius(stimradiusInitial)
+    targetstimuli.setAutoDraw(True)
     
-    continueExperiment = True
-    stimon = False
-    while continueExperiment:
+    elapsedTime.reset()
+    participantwin.flip(); #sendtrigger(5) # Send trigger            
+    stimulusisbeingdisplayed = True
+                          
+    while continueTrial:
+        turnstimoff = False # trial starts with stimulus on
         
-        if event.getKeys(["escape", "q"]): # Check for kill keys
-            continueExperiment = False
-            break
-    
-        if (runtimeTime.getTime() > float(durationinsec)):
-            continueExperiment = False
-        
-        if not stimon:
-            if (runtimeTime.getTime() > float(stimdelayinsec)) and (runtimeTime.getTime() < (float(stimdelayinsec) + float(stimdurationinsec))):
-                targetstimuli.setAutoDraw(True)
-                stimon = True
-                participantwin.flip()  
+        if (float(numpy.sum([elapsedTime.getTime(),refreshrate])) > float(trialdurationinsec)): # is the trial out of time
+            continueTrial = False
+            trial = trial + 1
+        else:
                 
-        if stimon:
-            if (runtimeTime.getTime() >= (float(stimdelayinsec) + float(stimdurationinsec))):
-                targetstimuli.setAutoDraw(False)
-                stimon = False
-                participantwin.flip()  
+            if stimulusisbeingdisplayed: 
+                # should stim be off
+                if (float(numpy.sum([elapsedTime.getTime(),refreshrate])) > float(stimdurationinsec)): # If stimulus duration has expired
+                    turnstimoff = True
+            
+            # To avoid tying RT to the monitor refresh rate, this loops for most of the refresh rate period
+            checkkeytimer = core.CountdownTimer(start=(float(numpy.multiply(float(0.8),float(refreshrate)))))
+            while (checkkeytimer.getTime() > 0):
+                
+                theseKeys = event.getKeys(keyList=participantkeys, timeStamped=cumulativeTime)
+                if (len(theseKeys) > 0): #at least one key was pressed
+                    responsetracking.append(theseKeys[0])
+                    #sendtrigger(theseKeys[0][0])
+                        
+                if event.getKeys(["escape", "q"]): # Check for kill keys
+                    continueExperiment = False
+                    continueTrial = False
+                    break   
+            
+        if turnstimoff:
+            targetstimuli.setAutoDraw(False)
+            stimulusisbeingdisplayed = False
+            participantwin.flip()
+                
+event.clearEvents()                          
+respmatrix  = pandas.DataFrame(columns = ['Event', 'Onset', 'Type'])
+for trial in range(0,len(responsetracking)):
+    respmatrix.at[trial,'Event'] = 'Response'
+    respmatrix.at[trial,'Onset'] = numpy.float32(responsetracking[trial][1])
+    respmatrix.at[trial,'Type'] = int(responsetracking[trial][0])                              
                 
 
 participantwin.close()
-del countTrials, targetstimuli, trialsequence
-del continueExperiment, durationinsec, participantwin, stimon, runtimeTime, stimdelayinsec, stimdurationinsec, stimradiusInitial
+del refreshrate, stimdurationinsec, trialdurationinsec, participantkeys
+del targetstimuli, trialsequence, continueTrial, continueExperiment, participantwin, turnstimoff, stimulusisbeingdisplayed, theseKeys, trial
+del stimradiusInitial
+del responsetracking
+
 print("Elapsed time: %.6f" % cumulativeTime.getTime())
 del  cumulativeTime
