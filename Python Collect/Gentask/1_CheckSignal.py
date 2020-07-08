4d1052c2af1ad9f199398f6b0c05fa661eff14fe
Matthew B. Pontifex <31024683+mattpontifex@users.noreply.github.com>
2020-07-08 14:40:35 -0400

    Add files via upload

---
+++ Python Collect/Gentask/1_CheckSignal.py
@@ -0,0 +1,12 @@
+from Engine.unicornhybridblackviewer import Viewer
+
+if __name__ == "__main__":
+    # Unicorn multiprocessing will not run in Spyder 
+    # Run -> Configuration per file... 
+    # Change the Console to Execute in an external system terminal
+    # Select the checkbox for External system terminal Interact with the Python console after execution
+    
+    task = Viewer()
+    task.channellabels = ['FCZ', 'CP1', 'CPZ', 'CP2', 'P1', 'PZ', 'P2', 'OZ']
+    task.unicorn = 'UN-2019.05.51' 
+    task.run()
