d84571930e26b5718cf6a78be630f5c007d5caa0
Matthew B. Pontifex <31024683+mattpontifex@users.noreply.github.com>
2021-10-07 14:35:46 -0400

    Add files via upload

--- Python Collect/Gentask/2_CheckPerformance.py
+++ Python Collect/Gentask/2_CheckPerformance.py
@@ -1,12 +1,14 @@
-from Engine.basicstimuluspresentationengine import Engine
 from Engine.checkperformance import performancereporter
 from tkinter import Tk     # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askopenfilename
 
+class Engine():
+    def __init__(self):
+        self.finished = True
+        
 if __name__ == "__main__":
 
     task = Engine()
-    task.finished = True
     Tk().withdraw() 
     filename = askopenfilename(filetypes=[("PythonCollect", "*.psydat"),("OddballTask", "OB*.psydat"), ("FlankerTask", "FT*.psydat"), ("N2backTask", "N2B*.psydat"), ("ContN2backTask", "CN2B*.psydat"), ("All", "*")])  # show an "Open" dialog box and return the path to the selected file
     task.outputfile = filename.split('.')[0] + '.psydat'
