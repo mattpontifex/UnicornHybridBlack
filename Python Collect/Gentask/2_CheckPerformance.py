4267292a752a1c244c1380fb25a421801180b2dd
Matthew B. Pontifex <31024683+mattpontifex@users.noreply.github.com>
2021-10-08 12:31:22 -0400

    Add files via upload

--- Python Collect/Gentask/2_CheckPerformance.py
+++ Python Collect/Gentask/2_CheckPerformance.py
@@ -10,9 +10,11 @@ if __name__ == "__main__":
 
     task = Engine()
     root = Tk()
+    root.lift()
+    root.wm_attributes('-topmost',1)
     root.withdraw()
     filename = askopenfilename(filetypes=[("PythonCollect", "*.psydat"),("OddballTask", "OB*.psydat"), ("FlankerTask", "FT*.psydat"), ("N2backTask", "N2B*.psydat"), ("ContN2backTask", "CN2B*.psydat"), ("All", "*")])  # show an "Open" dialog box and return the path to the selected file
     root.destroy()
-    task.outputfile = filename.split('.')[0] + '.psydat'
-    
-    performancereporter(task)
+    if len(filename) > 0:
+        task.outputfile = filename.split('.')[0] + '.psydat'
+        performancereporter(task)
