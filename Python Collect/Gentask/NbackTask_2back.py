954ccdb13fdb614fb2c4266797a8134ad993bffd
Matthew B. Pontifex <31024683+mattpontifex@users.noreply.github.com>
2021-11-22 11:33:33 -0500

    Add files via upload

--- Python Collect/Gentask/NbackTask_2back.py
+++ Python Collect/Gentask/NbackTask_2back.py
@@ -29,6 +29,7 @@ if __name__ == "__main__":
     
     # Usable keys during task
     task.participantkeys = ['enter', 'f', 'j']
+    task.participantprogressnotice = [0.1, 0.25, 0.5, 0.75, 0.9]
     
     # Experiment Flow Settings
     task.delaybeforestart = 3 # Seconds between end of instructions and beginning of task
--- Python Collect/Gentask/OddballTask.py
+++ Python Collect/Gentask/OddballTask.py
@@ -32,6 +32,7 @@ if __name__ == "__main__":
     
     # Experiment Flow Settings
     task.delaybeforestart = 2 # Seconds between end of instructions and beginning of task
+    task.participantprogressnotice = [0.1, 0.25, 0.5, 0.75, 0.9]
     
     # Modify Screen Settings for Participant
     task.participantmonitor.displaynumber = 0 # Monitor Number
